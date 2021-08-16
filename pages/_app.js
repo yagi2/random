171557a890cbae305e52c0c4b21861f7acdd05bc
Itsuki Aoyagi <mail@yagi2.dev>
2021-08-16 23:56:34 +0900

    enable code highlighting

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,11 @@
 import 'nextra-theme-blog/style.css'
+import Prism from 'prism-react-renderer/prism'
+
+(typeof global !== "undefined" ? global : window).Prism = Prism
+
+require("prismjs/components/prism-kotlin")
+require("prismjs/components/prism-swift")
+require("prismjs/components/prism-java")
 
 export default function Nextra({ Component, pageProps }) {
   return <><Component {...pageProps} /></>
